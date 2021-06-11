f4bf02ab00a2e7555104b8e26f3465f635f01ffd
agyllen <anton.gyllenhammar@gmail.com>
2021-06-11 11:50:11 +0200

    fix

--- index.php
+++ index.php
@@ -6,43 +6,11 @@
         <link href="./css/style.css" rel="stylesheet">
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
-        <script type="text/javascript">
-            console.log("Hello!")
-            // Wrap every word in a span
-            $('.sentence').each(function() {
-            let text = $(this).text();
-            let words = text.split(' ');
-
-            // Clear current element
-            this.innerHTML = '';
-
-            // Loop through each word, wrap each letter in a span
-            for (let word of words) {
-                //let word_split = word.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-                console.log('<span class="word">' + word + '</span>');
-
-                // Wrap another span around each word, add word to header
-                this.innerHTML += '<span class="word">' + word + '</span>';
-            }
-            });
-
-            anime.timeline({
-                loop: true
-            })
-            .add({
-                targets: '.sentence .word',
-                translateY: [110, 0],
-                opacity: [0, 1],
-                easing: "easeOutExpo",
-                duration: 1400,
-                delay: anime.stagger(250)
-            });
-        </script>
+        <script type="text/javascript" src="./scripts/script.js"></script>
     </head>
     <body>
+        <div class=bg></div>
         <div class=wrap>
-            <h2 class=sentence>test test test</h2>
             <?php
                 $PREFIX = getenv('PREFIX');
                 $NS = getenv('NAMESPACE');
