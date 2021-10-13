e4ab5d5eea9152f75bb85b497fe24d4fc2a653df
agyllen <anton.gyllenhammar@gmail.com>
2021-10-13 12:50:47 +0200

    postData test

--- html/js/script.js
+++ html/js/script.js
@@ -31,6 +31,11 @@ $(document).ready(function(e) {
             $("#adjective-info").html("required.");
             $("#adjective-input").addClass("input-error");
         }
+        postData('https://sentence-adjectives/adjectives', { name: adjective })
+        .then(data => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+
         return valid;
     });
 
@@ -153,6 +158,24 @@ function getSentence(){
     });
 }
 
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 
 
  
