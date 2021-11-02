6a23c32b8dbd8f5745f9ff0a9f7029dfe8909427
Anton Gyllenhammar <anton.gyllenhammar@gmail.com>
2021-11-02 16:39:00 +0000

    Development

--- html/js/script.js
+++ html/js/script.js
@@ -1,114 +1,89 @@
-
-$(document).ready(function(e) {
+$(document).ready(function(e) { 
     $('.bg').hide();
+    $('.sentence').hide();
+    $('.logos').hide();
+    getSentence();
+
+    
     $('<img/>').attr('src', 'https://picsum.photos/1920/1080?grayscale&blur=1').on('load', function() {
         $(this).remove(); // prevent memory leaks as @benweet suggested 
         $('.bg').css('background', 'linear-gradient(to top right, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.9)), url(https://picsum.photos/1920/1080?grayscale&blur=1) no-repeat center center fixed');
         $('.bg').css('background-size', 'cover');
         $('.bg').fadeIn(1000, function(){
-            wordAnimation(); 
+            $('.logos').fadeIn(1000)   
         });
-    });
-
-    // Wrap every word in a span   
-    $('.sentence').each(function() {
-        let text = $(this).text();
-        let words = text.split(' ');
-    
-        // Clear current element
-        this.innerHTML = '';
-    
-        // Loop through each word, wrap each letter in a span
-        for (let word of words) {
-            //let word_split = word.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-    
-            console.log('<span class="word">' + word + '</span>');
-    
-            // Wrap another span around each word, add word to header
-            this.innerHTML += '<span class="word">' + word + '</span>';
-        }
     });
 
 
     $(".plus-icon#adjective").click(function () {
-        $(".popup#adjective-popup").fadeIn(200);
-        
-    });
-    //Contact Form validation on click event
-    $("#adjective-form").on("submit", function () {
-        var valid = true;
-        $(".info").html("");
-        $("inputBox").removeClass("input-error");
+        showPrompt("Add an adjective", "adjective", "/api/sentence/adjectives", function(value) {
+            console.log("entered " + value);
 
-        var adjective = $("#adjective-input").val();
-
-        if (adjective == "") {
-            $("#adjective-info").html("required.");
-            $("#adjective-input").addClass("input-error");
-        }
-        return valid;
+        });
     });
 
     $(".plus-icon#animal").click(function () {
-        $(".popup#animal-popup").fadeIn(200);
-    });
-    //Contact Form validation on click event
-    $("#animal-form").on("submit", function () {
-        var valid = true;
-        $(".info").html("");
-        $("inputBox").removeClass("input-error");
-
-        var animal = $("#animal-input").val();
-
-        if (animal == "") {
-            $("#animal-info").html("required.");
-            $("#animal-input").addClass("input-error");
-        }
-        return valid;
-
+        showPrompt("Add an animal", "animal", "/api/sentence/animals", function(value) {
+            console.log("entered " + value);
+        }); 
     });
 
     $(".plus-icon#color").click(function () {
-        $(".poput#color-popup").show();
-    });
-    //Contact Form validation on click event
-    $("#color-form").on("submit", function () {
-        var valid = true;
-        $(".info").html("");
-        $("inputBox").removeClass("input-error");
-
-        var color = $("#color-input").val();
-
-        if (color == "") {
-            $("#color-info").html("required.");
-            $("#color-input").addClass("input-error");
-        }
-        return valid;
-
+        showPrompt("Add an color", "color", "/api/sentence/colors", function(value) {
+            console.log("entered " + value);
+        }); 
     });
 
     $(".plus-icon#location").click(function () {
-        $(".popup#location-popup").show();
-    });
-    //Contact Form validation on click event
-    $("#location-form").on("submit", function () {
-        var valid = true;
-        $(".info").html("");
-        $("inputBox").removeClass("input-error");
-
-        var location = $("#location-input").val();
-
-        if (location == "") {
-            $("#location-info").html("required.");
-            $("#location-input").addClass("input-error");
-        }
-        return valid;
-
+        showPrompt("Add an location", "location", "/api/sentence/locations", function(value) {
+            console.log("entered " + value);
+        }); 
     });
 
 });
 
+function showBanner(message, word, success){
+
+    $('.success-banner').removeClass("success failure");
+    $('#success-banner-message').html("");
+    $('#success-banner-image').removeClass();
+
+    if(success){
+        $('.success-banner').addClass("success");
+        $('#success-banner-message').html("Success! Your word " + word + " was added!" + message )
+        $('#success-banner-image').addClass("fas fa-check-circle")
+    } else {
+        $('.success-banner').addClass("failure");
+        $('#success-banner-message').html("Failure! Your word " + word + " was not added! " + message )
+        $('#success-banner-image').addClass("fas fa-times-circle")
+    }
+
+    bannerAnimation();
+}
+
+function bannerAnimation(){
+    
+    console.log("bannerAnimation");
+    $(".success-banner").show();
+
+    var bannerTimeline = anime.timeline({ 
+        targets: '.success-banner',
+        easing: "easeOutExpo",
+        duration: 1500,
+        delay: 1000,
+        endDelay: 5000,
+        loop: false
+    }).add({
+        translateY: ['-100%', '0%']
+    }).add({
+        translateY: ['0%', '-100%']
+    });
+}
+
 function wordAnimation(){
+
+    console.log("Word animation")
+    $('.sentence').show();
     
     var animTimeline = anime.timeline({
         loop: false
@@ -124,14 +99,142 @@ function wordAnimation(){
     });
 
     animTimeline.add({
-        targets: ['.plus-icon', '.success-banner'],
+        targets: ['.plus-icon'],
         opacity: [0, 1],
         easing: "easeOutExpo",
         duration: 1400,
     });
 }
 
+function getSentence(){
+    fetch('/api/sentence').then( response => {
+        console.log(response);
+        return response.json();
+    }).then( json => {
+        console.log(json);
+        $(".sentence#adjective").replaceWith("<h1 class=sentence id=adjective>" + json.adjectives + "</h1>" );
+        $(".sentence#animal").replaceWith("<h1 class=sentence id=animal>" + json.animals + "</h1>" );
+        $(".sentence#color").replaceWith("<h1 class=sentence id=color>" + json.colors + "</h1>" );
+        $(".sentence#location").replaceWith("<h1 class=sentence id=location>" + json.locations + "</h1>" );
 
+        // Wrap every word in a span for animation  
+        $('.sentence').each(function() {
+            let text = $(this).text();
+            let words = text.split(' ');
+        
+            // Clear current element
+            this.innerHTML = '';
+        
+            // Loop through each word, wrap each letter in a span
+            for (let word of words) {
+                //let word_split = word.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+        
+                // Wrap another span around each word, add word to header
+                this.innerHTML += '<span class="word">' + word + '</span>';
+            }
+        });
+        wordAnimation();
+    });
+}
 
- 
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+function showPrompt(text, word, post_uri, callback){
+    let form = document.getElementById('prompt-form');
+    let container = document.getElementById("prompt-form-container");
+    $("#prompt-message").html(text);
+    $(".info").html("");
+    $(".inputBox").removeClass("input-error");
+    form.text.value = "";
+
+    function complete(value) {
+        $("#prompt-form-container").fadeOut(200);
+        document.onkeydown = null;
+        callback(value);
+    };
+
+    form.onsubmit = function(event) {
+        console.log("submit");
+        $(".info").html("");
+        $(".inputBox").removeClass("input-error");
+
+        let value = form.text.value;
+
+        value = sanitizeString(value);
+        console.log(value);
+        if (value == "" ) {
+            $(".info").html("Required");
+            $(".inputBox").addClass("input-error");
+            return false;
+        }
+
+        postData(post_uri, { "value": value })
+            .then(data => {
+                console.log(data); // JSON data parsed by `data.json()` call
+                if (data.accepted == "true") {
+                    console.log("accepted");
+                    showBanner(data.info, data.value, true);
+                } else {
+                    console.log("not accepted");
+                    showBanner(data.info, data.value, false);
+                };
+            })
+        ;
+        
+        complete(value);
+        return false;
+    };
+
+    form.cancel.onclick = function() {
+        complete(null);
+    };
+
+    document.onkeydown = function(e) {
+        if (e.key == 'Escape') {
+            complete(null);
+        }
+    };
+
+    let lastElem = form.elements[form.elements.length - 1];
+    let firstElem = form.elements[0];
+
+    lastElem.onkeydown = function(e) {
+        if (e.key == 'Tab' && !e.shiftKey) {
+            firstElem.focus();
+            return false;
+        }
+    };
+
+    firstElem.onkeydown = function(e) {
+        if (e.key == 'Tab' && e.shiftKey) {
+            lastElem.focus();
+            return false;
+        }
+    };
+
+    $("#prompt-form-container").fadeIn(200);
+    form.elements.text.focus();
+}
+
+function sanitizeString(str){
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+    return str.trim();
+} 
   
